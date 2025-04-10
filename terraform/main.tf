a96e9bc5d0bd94159d1eafc76106aae6d6edd3b5
stutig123 <stuti.gambhir123@gmail.com>
2025-04-10 23:34:17 +0530

    Add GitHub Actions pipeline and Ansible deployment setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,59 +1,37 @@
 provider "aws" {
-  region = "ap-south-1"
-}
-
-resource "tls_private_key" "jobportal_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
+  region     = "ap-south-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_key_pair" "jobportal_key" {
-  key_name   = "jobportal-key"
-  public_key = tls_private_key.jobportal_key.public_key_openssh
+  key_name   = "stuti-jobportal-key"
+  public_key = file("${path.module}/stuti-jobportal-key.pub")
 }
 
-resource "aws_instance" "jobportal" {
-  ami           = "ami-0a7cf821b91bcccbc" # Ubuntu 22.04 LTS in ap-south-1
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.jobportal_key.key_name
+resource "aws_instance" "jobportal_instance" {
+  ami                         = "ami-03f4878755434977f" # Ubuntu 22.04 LTS in ap-south-1
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.jobportal_key.key_name
+  associate_public_ip_address = true
 
   tags = {
-    Name = "JobPortalEC2"
+    Name = "JobPortalApp"
   }
 
-  provisioner "local-exec" {
-    command = "echo '${tls_private_key.jobportal_key.private_key_pem}' > stuti-jobportal-key.pem && chmod 400 stuti-jobportal-key.pem"
+  provisioner "file" {
+    source      = "stuti-jobportal-key.pem"
+    destination = "/home/ubuntu/stuti-jobportal-key.pem"
   }
 
-  security_groups = [aws_security_group.jobportal_sg.name]
-}
-
-resource "aws_security_group" "jobportal_sg" {
-  name        = "jobportal-sg"
-  description = "Allow HTTP and SSH"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("stuti-jobportal-key.pem")
+    host        = self.public_ip
   }
 }
 
 output "public_ip" {
-  value = aws_instance.jobportal.public_ip
+  value = aws_instance.jobportal_instance.public_ip
 }
